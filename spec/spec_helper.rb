acf180db133e1465fcdb4c4314ed23a0409fff29
Neill Turner <neillwturner@gmail.com>
2016-03-10 11:05:37 +0000

    use ansiblespec_runner script

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ RSpec.configure do |config|
   # ssh via password, set :version to :debug for debugging
   #set :ssh_options, :user => ENV['LOGIN_USER'], :paranoid => false, :verbose => :info, :password => ENV['LOGIN_PASSWORD'] if ENV['LOGIN_PASSWORD']
   # ssh via ssh key
-  set :ssh_options, :user => ENV['LOGIN_USER'], :paranoid => false, :verbose => :info, :host_key => 'ssh-rsa', :keys => [ ENV['SSH_KEY'] ] if ENV['SSH_KEY']
+  set :ssh_options, :user => ENV['LOGIN_USER'], :paranoid => false, :verbose => :error, :host_key => 'ssh-rsa', :keys => [ ENV['SSH_KEY'] ] if ENV['SSH_KEY']
   set :backend, :ssh
   set :request_pty, true
 end
